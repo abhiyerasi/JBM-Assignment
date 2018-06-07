60f8f7a435c531109d11d40fae6b48f56a0d318c
abhiyerasi <abhilashreddynaukri@gmail.com>
2018-06-07 12:51:32 +0530

    JBM-Assignment

--- jbm_model.py
+++ jbm_model.py
@@ -1,22 +1,23 @@
 
 # coding: utf-8
+# Model Building and Weight
 
-# In[16]:
-
-
+# import the required libraries
 import numpy as np
 import pandas as pd
 
 
-# In[17]:
-
-
+# import the keras libraries
 from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D,Dropout
 from keras.layers import Flatten
 from keras.layers import Dense
 from PIL import Image
+from keras.models import Model
+from keras.preprocessing.image import ImageDataGenerator
+
+# model archeticture desgin
 
 classifier = Sequential()
 
@@ -32,9 +33,9 @@ classifier.add(Convolution2D(128, 3, 3, input_shape = (150, 150, 3), activation 
 classifier.add(Dropout(0.1))
 classifier.add(MaxPooling2D(pool_size = (2,2)))
 
+# the model so far outputs 3D feature maps (height, width, features)
 
-# In[18]:
-
+# Flatten out the 3D feature into 1D feature vectors
 
 classifier.add(Flatten())
 
@@ -44,32 +45,39 @@ classifier.add(Dense(output_dim = 128, activation = 'relu'))
 classifier.add(Dropout(0.4))
 classifier.add(Dense(output_dim = 6, activation = 'sigmoid'))
 
+# Put the output dimenssion as the number of class variables
 classifier.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['categorical_accuracy'])
 
 
-# In[19]:
-
-
-from keras.preprocessing.image import ImageDataGenerator
+# this is the augmentation configuration we will use for training
 train_datagen = ImageDataGenerator(
         rescale=1./255,
         shear_range=0.2,
         zoom_range=0.2,
         horizontal_flip=True)
 
+# this is the augmentation configuration we will use for testing:
+# only rescaling
 test_datagen = ImageDataGenerator(rescale=1./255)
 
+
+# this is a generator that will read pictures found in
+# subfolers of './data/train_61326', and indefinitely generate
+# batches of augmented image data
 training_set = train_datagen.flow_from_directory(
-        'E:/JBM/trying/data/train_61326',
+        './data/train_61326',
         target_size=(150, 150),
         batch_size=32,
         class_mode='categorical')
+
+# this is a similar generator, for validation data
 validation_generator = train_datagen.flow_from_directory(
-        'E:/JBM/trying/data/validation_61326',  # this is the target directory
+        './data/validation_61326',  
         target_size=(150, 150),  # all images will be resized to 150x150
         batch_size=32,
-        class_mode='categorical')  # since we use binary_crossentropy loss, we need binary labels
+        class_mode='categorical')  # since we use Categorical_crossentropy loss, we need categorical labels
 
+# Fit the model and generate the classifier
 classifier.fit_generator(training_set,
                          steps_per_epoch=32,
                          epochs=3,
@@ -77,11 +85,7 @@ classifier.fit_generator(training_set,
                          validation_steps=10)
 
 
-# In[15]:
-
-
-from keras.models import Model
-# serialize weights to HDF5
+# serialize weights to HDF5 and save them for future references
 classifier.save_weights("modelConv2d.h5")
 classifier.save('model.h5')
 print("Saved model to disk")
--- predict.py
+++ predict.py
@@ -1,18 +1,17 @@
 
 # coding: utf-8
+# Predict the Image classes Using the weightes stored in the folder
 
-# In[33]:
-
-
+# Import the Required Libraries
 import os
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from keras.models import Sequential, load_model
+import numpy as np
+from keras.preprocessing import image
 
 
-# In[37]:
-
-
+# Basic Settings
 img_width, img_height = 150, 150
 model_path = 'model.h5'
 model_weights_path = 'modelConv2d.h5'
@@ -20,41 +19,32 @@ model = load_model(model_path)
 model.load_weights(model_weights_path)
 
 
-# In[38]:
-
-
-from keras.preprocessing.image import ImageDataGenerator
+# Rescale the Image and store it in a folder
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 
 test_set = test_datagen.flow_from_directory(
-    'E:/JBM/trying/test-data/test_61326',
+    './test-data/test_61326',
     target_size=(150, 150),
     batch_size=16,
     class_mode=None,
 shuffle=False)
 
 
-# In[46]:
+# List all the files in the directory
+list_file=os.listdir("./test-data/test_61326/")
 
-
-import os
-list_file=os.listdir("E:/JBM/trying/test-data/test_61326/")
-
-
-# In[48]:
 
 
 ##Predicting using test data
-import numpy as np
-from keras.preprocessing import image
 for i in list_file:
     stri = str(i)
-    test_image = image.load_img('E:/JBM/trying/test-data/test_61326/' + stri, target_size = (150,150))
+    test_image = image.load_img('./test-data/test_61326/' + stri, target_size = (150,150))
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image,axis = 0)
-    result = model.predict_classes(test_image,verbose=1)
+    result = model.predict_classes(test_image,verbose=1) # Predicts the class labels
     
+    # Convert the predicted class into label and preidct it
     if result==0:
         print('61326_ok_back')
     elif result==1:
